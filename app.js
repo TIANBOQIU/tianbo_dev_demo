458969a758979cf2453743bccde1e25a1083015c
= <doublelockcup@gmail.com>
2019-11-29 16:15:53 -0500

    export httpserver

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const multer = require("multer");
 const ejs = require("ejs");
 const path = require("path");
-const app = express();
+const app = (module.exports.app = express());
+const http = require("http");
+const https = require("https");
+const httpServer = (module.exports.httpServer = http.createServer(app));
+
 const port = 3000;
 
 const fileUpload = require("./controllers/upload");
@@ -15,9 +19,10 @@ const fileUpload = require("./controllers/upload");
 app.set("view engine", "ejs");
 // public static folder
 //app.use(express.static("public"));
+
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("index"));
 app.get("/upload", (req, res) => res.render("upload"));
 app.post("/upload", fileUpload);
 
-app.listen(port, () => console.log(`server started on port ${port}`));
+httpServer.listen(port, () => console.log(`server started on port ${port}`));
